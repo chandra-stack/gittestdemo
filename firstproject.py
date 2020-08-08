d490a7adc49a99b6bba89ef47ed2bc6b89bf121e
chandra-stack <chandraiahmo@gmail.com>
2020-08-08 15:09:11 +0530

    indian team

--- firstproject.py
+++ firstproject.py
@@ -4,7 +4,12 @@ class Chandra:
         self.a = a
         self.b = b
         print("i will be calling when object created")
+        #american guy has to do this task and it starts here.
         print("im from american team and doing some recoding")
+        #and it ends here.
+        # indian guy has to do this task and it starts here.
+        print("im from indian team and doing recoding back")
+        #and it ends here.
 
     def sum(self):
         return self.a + self.b
